dbab1b89e7b424a4f1a78fa4a6b903e182a207c2
Cole Smith <114085318+colesmith54@users.noreply.github.com>
2024-03-20 02:19:39 +0000

    init

--- src/app.ts
+++ src/app.ts
@@ -27,28 +27,6 @@ const PORT = 3000;
 
 app.use(cors());
 
-const allowCors = (fn: { (req: any, res: any): void; (arg0: any, arg1: any): any; }) => async (req: { method: string; }, res: { setHeader: (arg0: string, arg1: string | boolean) => void; status: (arg0: number) => { (): any; new(): any; end: { (): void; new(): any; }; }; }) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req: any, res: { end: (arg0: string) => void; }) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-
 
 router.get("/marker", async (req: any, res: any) => {
   const marker = req.query as Marker;
@@ -97,5 +75,3 @@ app.listen(PORT, (error: any) => {
     console.log("Error occurred, server can't start", error);
   }
 });
-
-module.exports = allowCors(handler)
